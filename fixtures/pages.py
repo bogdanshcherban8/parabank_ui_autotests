b6f0d37478d4a9d684c4a35fc8762795aa9f31de
bogdanshcherban8 <bogdanshcherban8@gmail.com>
2025-09-03 18:14:19 +0400

    architecture is done

--- fixtures/pages.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from playwright.sync_api import Page
-from pages.index_page.index_page import IndexPage
-
-
-@pytest.fixture
-def index_page(browser_page:Page)->IndexPage:
-    return IndexPage(page=browser_page)
--- tools/jsessionid.py
+++
@@ -1,6 +0,0 @@
-def get_jsessionid(page):
-    cookies = page.context.cookies()
-    for cookie in cookies:
-        if cookie["name"].upper() == "JSESSIONID":
-            return cookie["value"]
-    return None
